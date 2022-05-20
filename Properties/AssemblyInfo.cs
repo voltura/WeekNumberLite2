201d4a9ebf1a24c68c818d2805bbed8187b12ee2
Joakim Skoglund <joakim@voltura.se>
2022-05-20 10:15:43 +0200

    Updated version to 1.0.0.4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("88D1C6E0-96C8-4BB3-BA08-ACEFDC90A023")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
 #endregion Assembly information
