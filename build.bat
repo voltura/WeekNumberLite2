7b68b7abb25ac1267ff2498f612e8c94aacab2b6
Joakim Skoglund <joakim@voltura.se>
2023-01-26 20:17:39 +0100

    Updated msbuild and dotnet versions used for release script

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" WeekNumberLite2.sln /p:Platform=x64 /t:Clean,Build /property:Configuration=Release -maxcpucount
-"C:\Program Files\Microsoft Visual Studio\2022\Community\dotnet\runtime\dotnet.exe" publish -r win-x64 -c Release
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" WeekNumberLite2.sln /p:Platform=x64 /t:Clean,Build /property:Configuration=Release -maxcpucount
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\dotnet\runtime\dotnet.exe" publish -r win-x64 -c Release
